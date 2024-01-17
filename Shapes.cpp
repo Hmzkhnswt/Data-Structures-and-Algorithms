28a6136ba97f25c490e595db2c6e4c1629e5eaea
Hamza Ali <95092387+Hmzkhnswt@users.noreply.github.com>
2024-01-17 11:28:28 +0500

    Add files via upload

---
+++ Shapes.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+//	for(int i=1; i<=6; i++){
+//		for(int j=1; j<=i; j++){
+//			cout<<"8";
+//		}
+//		cout<<endl;
+//	}
+	
+	for(int i=6; i<=1; i++){
+		for(int j=6; j<=i; j++){
+			cout<<"8";
+		}
+		cout<<endl;
+	}
+	
+	
+	return 0;
+}
